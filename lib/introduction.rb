2f8c5103026f05922719d7549b164e374c762716
Michael Lynch <michael@michaelrlynch.com>
2019-08-06 00:34:38 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduntion(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduntion_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
